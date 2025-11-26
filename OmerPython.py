e6620db4c161a37c5c82571cbefda2c098541a5c
OMER <girgineromer3019@gmail.com>
2025-11-26 11:01:12 +0100

    Update for Python-codes
    Ik heb mijn codes goed gemaakt

--- OmerPython.py
+++ OmerPython.py
@@ -1,11 +1,11 @@
 
 
-#Python f-Strings Example
+##################### Python f-Strings Example###################
 age = 36
 txt = f"My name is John, I am {age}"
 print(txt)
 
-#Comparison Example
+##################### Comparison Example###################
 a = 200
 b = 33
 
@@ -14,7 +14,7 @@ if b > a:
 else:
   print("b is not greater than a")
 
-#Arithmetic Operations Example
+##################### Arithmetic Operations Example###################
 x = 15
 y = 4
 
@@ -26,29 +26,29 @@ print(x % y)
 print(x ** y)
 print(x // y)
 
-#Python Lists Example
+##################### Python Lists Example###################
 fruits = ["apple", "banana", "cherry"]
 for fruit in fruits:
     print(fruit)
 
-#List removal example
+#################### List removal example ####################
 fruits = ["apple", "banana", "cherry"]
 fruits.remove("banana")
 for fruit in fruits:
     print(fruit)
 
-#List comprehension example
+################### List comprehension example ###################
 fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 newlist = [x for x in fruits if "c" in x]
 print(newlist)
 output: ['cherry']
 
-#List iterable example
+################### List iterable example ###################
 newlist = [x for x in range(10) if x < 5]
 print(newlist)
 output: [0, 1, 2, 3, 4]
 
-#Add Itemes to List Example
+################### Add Itemes to List Example ##############
 fruits = ["apple", "banana", "cherry"]
 fruits.append("orange")
 for fruit in fruits:
@@ -59,7 +59,7 @@ banana
 cherry
 orange
 
-#Add Sets Example
+################# Add Sets Example###############
 thisset = {"apple", "banana", "cherry"}
 tropical = {"pineapple", "mango", "papaya"}
 
@@ -67,20 +67,26 @@ thisset.update(tropical)
 print(thisset)
 output: {'banana', 'cherry', 'papaya', 'mango', 'apple', 'pineapple'}
 
-#Remove Item
+################ Remove Item##############
 thisset = {"apple", "banana", "cherry"}
 thisset.remove("banana")
 print(thisset)
 output: {'cherry', 'apple'}
 
-#Intersection 
+############## Intersection##############
 set1 = {"apple", "banana", "cherry"}
 set2 = {"google", "microsoft", "apple"}
 set3 = set1.intersection(set2)
 print(set3)
 output: {'apple'}
 
-# my_module.py
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1 & set2
+print(set3)
+output: {'apple'}
+
+############# my_module.py#################
 def greet(name):
     return f"Hello, {name}!"
 
