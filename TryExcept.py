d888ce32f5fc148201cb3cca94469d26713c9c00
OMER <girgineromer3019@gmail.com>
2025-12-04 14:19:49 +0100

    Update TryExcept
    Ik heb een voorbeleid toegevoed voor TryExcept-voorbeleid

--- TryExcept.py
+++ TryExcept.py
@@ -16,3 +16,16 @@ except ValueError:
     print("Jij hebt geen geldig getal opgegeven!")
 """
 #---------------------------------------------#
+
+"""
+try:
+    c = int(input("Geef een getal op: "))
+    resultaat = 100 / c
+    print("100 gedeeld door {} is {}".format(c, resultaat))
+except ValueError:
+    print("Jij hebt geen geldig getal opgegeven!")
+except ZeroDivisionError:
+    print("Delen door nul is niet toegestaan!")
+"""
+
+#---------------------------------------------#
