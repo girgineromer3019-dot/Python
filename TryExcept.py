06969766ddf4414a7bc82f4f9ce6f1fb2fbb6465
OMER <girgineromer3019@gmail.com>
2025-12-04 12:51:10 +0100

    Update TryExcept
    I added a new example for tryexcept-file

--- TryExcept.py
+++ TryExcept.py
@@ -1,7 +1,18 @@
 # Deze file om try-except blokken te leren en testen #
-
+#---------------------------------------------#
+"""
 try:
     a = int("kocpuasipu3289rw9inoasv")
     print(a)
 except ValueError:
     print("Fout")
+"""
+#---------------------------------------------#
+"""
+try:
+    b = int(input("Geef een getal op: "))
+    print("Je hebt het getal {} opgegeven.".format(b))
+except ValueError:
+    print("Jij hebt geen geldig getal opgegeven!")
+"""
+#---------------------------------------------#
