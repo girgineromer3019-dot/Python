71eee1bc233ecb0e5d17f268c2df4d36723c5918
OMER <girgineromer3019@gmail.com>
2025-12-05 13:29:45 +0100

    Update TryExcept
    I tryed new think about TryExcept

--- TryExcept.py
+++ TryExcept.py
@@ -1,5 +1,6 @@
 # Deze file om try-except blokken te leren en testen #
 #---------------------------------------------#
+
 """
 try:
     a = int("kocpuasipu3289rw9inoasv")
@@ -7,7 +8,9 @@ try:
 except ValueError:
     print("Fout")
 """
+
 #---------------------------------------------#
+
 """
 try:
     b = int(input("Geef een getal op: "))
@@ -15,6 +18,7 @@ try:
 except ValueError:
     print("Jij hebt geen geldig getal opgegeven!")
 """
+
 #---------------------------------------------#
 
 """
@@ -29,3 +33,15 @@ except ZeroDivisionError:
 """
 
 #---------------------------------------------#
+
+"""
+try:
+    x = int(input("Geef een getal op:"))
+    print("Je hebt het getal {} opgegeven.".format(x))
+except (ValueError, Exception):
+    print("Foutmelding:", Exception, ZeroDivisionError)
+print("Einde van het programma.")
+"""
+
+#---------------------------------------------#
+
