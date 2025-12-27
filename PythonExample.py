aa693f091a4fb6b9cb1986ac2d72059893b817ba
OMER <girgineromer3019@gmail.com>
2025-12-28 00:01:54 +0100

    Create PythonExample
    I added a new file for exercies

---
+++ PythonExample.py
@@ -0,0 +1,9 @@
+class file():
+    def __init__(self):
+        with open("TvRemote.py", "r") as f:
+            FileContent = f.read()
+            words = FileContent.split()
+            for i in words:
+                print(i)
+
+file = file()
